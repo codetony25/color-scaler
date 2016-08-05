9f05629ea659a5fc271f4d90c209b9454d15a9dc
Tony Nguyen <tony.nguyen@doclermedia.com>
2016-08-05 11:40:47 -0700

    package.json

--- server.js
+++ server.js
@@ -13,3 +13,19 @@ new WebpackDevServer(webpack(config), {
 
   console.log('Listening at localhost:8080');
 });
+
+
+// var express = require('express');
+// var path = require('path');
+//
+// var app = express();
+//
+// var isProduction = process.env.NODE_ENV === 'production';
+// var port = isProduction ? process.env.PORT : 3000;
+// var publicPath = path.resolve(__dirname, 'dist');
+//
+// app.use(express.static(publicPath));
+//
+// app.listen(port, function () {
+//   console.log('Server running on port ' + port);
+// });
