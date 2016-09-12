9b8d31f4269f35e3ed9fa6ad9a52161b2f7d7e00
Tony Nguyen <tonytainguyen25@gmail.com>
2016-09-11 18:20:57 -0700

    changed ports

--- bin/server.js
+++ bin/server.js
@@ -12,10 +12,10 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(process.env.PORT || 8080, function (err, result) {
+}).listen(process.env.PORT || 6000, function (err, result) {
   if (err) {
     console.log(err);
   }
 
-  console.log('Listening at localhost:8080');
+  console.log('Listening at localhost:6000');
 });
